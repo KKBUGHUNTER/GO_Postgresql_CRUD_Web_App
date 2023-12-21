c8b79670693bfea00a6afdd0e3737d654c0334a4
CYBER_KARTHIK <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-12-21 15:12:54 +0530

    Add files via upload

---
+++ SQLHelper/initQuery.sql
@@ -0,0 +1,9 @@
+CREATE TABLE student(
+    studentname VARCHAR(255) PRIMARY KEY,
+    class INT,
+    mark INT
+);
+
+INSERT INTO student VALUES('Karthikeyan', 11, 195);
+INSERT INTO student VALUES('Sri Kanth', 11, 183);
+INSERT INTO student VALUES('Kumar', 11, 199);
